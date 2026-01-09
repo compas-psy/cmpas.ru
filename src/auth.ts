4a4fe029afda3a1b43e3f987801bd5c17c4d6d7f
Eliah <eliah.n.martynov@gmail.com>
2026-01-10 02:14:44 +0300

    fix(auth): update env config and optional smtp auth

--- src/auth.ts
+++ src/auth.ts
@@ -14,11 +14,13 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
         Nodemailer({
             server: {
                 host: process.env.EMAIL_SERVER_HOST,
-                port: Number(process.env.EMAIL_SERVER_PORT) || 25,
-                auth: (process.env.EMAIL_SERVER_USER && process.env.EMAIL_SERVER_PASSWORD) ? {
-                    user: process.env.EMAIL_SERVER_USER,
-                    pass: process.env.EMAIL_SERVER_PASSWORD,
-                } : undefined,
+                port: Number(process.env.EMAIL_SERVER_PORT),
+                ...(process.env.EMAIL_SERVER_USER && {
+                    auth: {
+                        user: process.env.EMAIL_SERVER_USER,
+                        pass: process.env.EMAIL_SERVER_PASSWORD,
+                    },
+                }),
             },
             from: process.env.EMAIL_FROM || "noreply@cmpas.ru",
         }),
